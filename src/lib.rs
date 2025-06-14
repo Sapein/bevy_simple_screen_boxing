cc20e8d3f87dcc717124ff60efe2f2e68c215a36
Sapein <5852983+Sapein@users.noreply.github.com>
2025-06-13 20:02:57 -0500

    Adjust how calculating sizes for int scales works.

--- src/lib.rs
+++ src/lib.rs
@@ -22,28 +22,31 @@ pub struct CameraBox {
 
 #[derive(Reflect)]
 pub enum CameraBoxMode {
-    StaticSize {
+    StaticResolution {
         resolution: UVec2,
         position: Option<UVec2>,
     },
-    // StaticAspectRatio(AspectRatio),
+    StaticAspectRatio {
+        aspect_ratio: AspectRatio,
+        position: Option<UVec2>,
+    },
     ResolutionIntegerScale {
         allow_imperfect_aspect_ratios: bool,
+        resolution: Vec2,
     },
     LetterBox { top_size: UVec2, bottom_size: UVec2 },
     PillarBox { top_size: UVec2, bottom_size: UVec2 },
 }
-
 fn adjust_viewport(
-    mut boxed_cameras: Query<(&mut Camera, &Projection, &CameraBox)>,
+    mut boxed_cameras: Query<(&mut Camera, &CameraBox)>,
     primary_window: Query<Option<Entity>, With<PrimaryWindow>>,
     windows: Query<(Entity, &Window)>,
     texture_views: Res<ManualTextureViews>,
     images: Res<Assets<Image>>,
 ) {
-    for (mut camera, projection, camera_box) in boxed_cameras.iter_mut() {
+    for (mut camera, camera_box) in boxed_cameras.iter_mut() {
         match &camera_box.mode {
-            CameraBoxMode::StaticSize { resolution: size, position, } => match &mut camera.viewport {
+            CameraBoxMode::StaticResolution { resolution: size, position, } => match &mut camera.viewport {
                 Some(viewport) => {
                     if &viewport.physical_size != size {
                         if size.x > viewport.physical_size.x || size.y > viewport.physical_size.y {
@@ -73,7 +76,10 @@ fn adjust_viewport(
                     })
                 }
             },
-            CameraBoxMode::ResolutionIntegerScale { allow_imperfect_aspect_ratios }=> {
+            CameraBoxMode::StaticAspectRatio { aspect_ratio, position } => match &mut camera.viewport {
+                _ => todo!()
+            },
+            CameraBoxMode::ResolutionIntegerScale { allow_imperfect_aspect_ratios, resolution }=> {
                 let target = camera.target.normalize(
                     primary_window
                         .iter()
@@ -88,50 +94,49 @@ fn adjust_viewport(
                     Some(target) => target
                 };
 
-                if !allow_imperfect_aspect_ratios {
-                    todo!()
-                }
-
-                match projection {
-                    Projection::Perspective(_) => todo!(),
-                    Projection::Orthographic(projection) => {
-                        match projection.scaling_mode {
-                            ScalingMode::WindowSize => continue,
-                            ScalingMode::Fixed { .. } => todo!(),
-                            ScalingMode::AutoMin { .. } => todo!(),
-                            ScalingMode::AutoMax { max_height: desired_height, max_width: desired_width } => {
-                                let (boxing, sizing) = match calculate_sizes_imperfect(target.physical_size, (desired_width, desired_height)) {
-                                    Ok(opt) => match opt {
-                                        None => {
-                                            camera.viewport = None;
-                                            continue;
-                                        }
-                                        Some((b, s)) => (b, s),
-                                    }
-                                    Err(_) => continue,
-                                };
-                                let mut port = Viewport {
-                                    physical_position: boxing,
-                                    physical_size: sizing,
-                                    ..default()
-                                };
-                                camera.viewport = Some(port);
+                let (boxing, sizing) =  if *allow_imperfect_aspect_ratios {
+                    match calculate_sizes_imperfect(&target.physical_size.as_vec2(), resolution) {
+                        Ok(opt) => match opt {
+                            None => {
+                                camera.viewport = None;
+                                continue;
                             }
-                            ScalingMode::FixedVertical { .. } => todo!(),
-                            ScalingMode::FixedHorizontal { .. } => todo!(),
+                            Some(t) => t,
                         }
+                        Err(_) => continue,
                     }
-                    Projection::Custom(_) => todo!(),
-                }
+                } else {
+                    match calculate_sizes_perfect(&target.physical_size.as_vec2(), resolution) {
+                        Ok(opt) => match opt {
+                            None => {
+                                camera.viewport = None;
+                                continue;
+                            }
+                            Some(t) => t,
+                        }
+                        Err(_) => continue,
+                    }
+                };
+
+                camera.viewport = Some(Viewport {
+                    physical_position: boxing.as_uvec2(),
+                    physical_size: sizing.as_uvec2(),
+                    ..default()
+                });
+            },
+            CameraBoxMode::LetterBox { top_size, bottom_size }  => {
+                todo!()
+            },
+            CameraBoxMode::PillarBox { top_size, bottom_size } => {
+                todo!()
             }
-            _ => todo!(),
         }
     }
 }
 
-fn calculate_sizes_imperfect(physical_size: UVec2, (desired_width, desired_height): (f32, f32)) -> Result<Option<(UVec2, UVec2)>, ()> {
-    let desired_aspect_ratio = AspectRatio::try_new(desired_width, desired_height);
-    let physical_aspect_ratio = AspectRatio::try_new(physical_size.x as f32, physical_size.y as f32);
+fn calculate_sizes_imperfect(physical_size: &Vec2, desired_size: &Vec2) -> Result<Option<(Vec2, Vec2)>, ()> {
+    let desired_aspect_ratio = AspectRatio::try_from(*desired_size);
+    let physical_aspect_ratio = AspectRatio::try_from(*physical_size);
     if desired_aspect_ratio.is_err() || physical_aspect_ratio.is_err() {
         return Err(());
     }
@@ -140,11 +145,11 @@ fn calculate_sizes_imperfect(physical_size: UVec2, (desired_width, desired_heigh
     let physical_ar = physical_aspect_ratio.unwrap();
 
     //NOTE: this does not really handle the case where the target size is smaller than the desired height/width.
-    let height_scale = physical_size.y as f32 / desired_height;
-    let width_scale = physical_size.x as f32 / desired_width;
+    let height_scale = physical_size.y / desired_size.y;
+    let width_scale = physical_size.x / desired_size.x;
 
-    let small_height_scale = desired_height / physical_size.y as f32;
-    let small_width_scale = desired_height / physical_size.y as f32;
+    let small_height_scale = desired_size.y / physical_size.y;
+    let small_width_scale = desired_size.x / physical_size.x;
 
     let has_int_scale = desired_ar.ratio() == physical_ar.ratio() && (
         (height_scale % 1. == 0. && width_scale % 1. == 0.) || (small_height_scale % 1. == 0. && small_width_scale % 1. == 0.)
@@ -162,19 +167,83 @@ fn calculate_sizes_imperfect(physical_size: UVec2, (desired_width, desired_heigh
     };
 
     let render_width = if best_scale >= 1. {
-        desired_width * best_scale.floor()
+        desired_size.x * best_scale.floor()
     } else {
-        desired_width * best_scale
+        desired_size.x * best_scale
     };
 
     let render_height = if best_scale >= 1. {
-        desired_height * best_scale.floor()
+        desired_size.y * best_scale.floor()
     } else {
-        desired_height * best_scale
+        desired_size.y * best_scale
     };
 
     let letterbox_size = physical_size.y as f32 - render_height;
     let pillarbox_size = physical_size.x as f32 - render_width;
 
-    Ok(Some((Vec2::new(pillarbox_size / 2. , letterbox_size / 2.).as_uvec2(), Vec2::new(desired_width, desired_height).as_uvec2())))
+    Ok(Some((Vec2::new(pillarbox_size / 2., letterbox_size / 2.), Vec2::new(render_width, render_height))))
+}
+
+fn calculate_sizes_perfect(physical_size: &Vec2, desired_size: &Vec2) -> Result<Option<(Vec2, Vec2)>, ()> {
+    let desired_aspect_ratio = AspectRatio::try_from(*desired_size);
+    let physical_aspect_ratio = AspectRatio::try_from(*physical_size);
+    if desired_aspect_ratio.is_err() || physical_aspect_ratio.is_err() {
+        return Err(());
+    }
+
+    let desired_ar = desired_aspect_ratio.unwrap();
+    let physical_ar = physical_aspect_ratio.unwrap();
+
+    //NOTE: this does not really handle the case where the target size is smaller than the desired height/width.
+    let height_scale = physical_size.y / desired_size.y;
+    let width_scale = physical_size.x / desired_size.x;
+
+    let has_int_scale = desired_ar.ratio() == physical_ar.ratio() && (
+        (height_scale % 1. == 0. && width_scale % 1. == 0.)
+    );
+
+    // Integer Scaling Exists
+    if has_int_scale {
+        return Ok(None);
+    }
+
+    if height_scale < 1. || width_scale < 1. {
+        let height_scale = desired_size.y / physical_size.y;
+        let width_scale = desired_size.x / physical_size.x;
+
+        let best_divisor = if height_scale < width_scale {
+            width_scale
+        } else {
+            height_scale
+        }.ceil();
+
+        let render_height = desired_size.y / best_divisor;
+        let render_width = desired_size.x / best_divisor;
+
+        let letterbox_size = physical_size.y - render_height;
+        let pillarbox_size = physical_size.x - render_width;
+        Ok(Some((Vec2::new(pillarbox_size / 2., letterbox_size / 2.), Vec2::new(render_width, render_height))))
+    } else {
+        let best_scale = if width_scale > height_scale {
+            height_scale
+        } else {
+            width_scale
+        };
+
+        let render_width = if best_scale >= 1. {
+            desired_size.x * best_scale.floor()
+        } else {
+            desired_size.x * best_scale
+        };
+
+        let render_height = if best_scale >= 1. {
+            desired_size.y * best_scale.floor()
+        } else {
+            desired_size.y * best_scale
+        };
+
+        let letterbox_size = physical_size.y - render_height;
+        let pillarbox_size = physical_size.x - render_width;
+        Ok(Some((Vec2::new(pillarbox_size / 2., letterbox_size / 2.), Vec2::new(render_width, render_height))))
+    }
 }
